2f096bf080f0cb686d46911823d73936ce968be5
tlp19 <tlp19@ic.ac.uk>
2022-02-13 00:30:43 +0000

    cleanup attempt at local_notifications

--- lib/main.dart
+++ lib/main.dart
@@ -3,43 +3,6 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:return_success_4_app/view/homepage/homepage.dart';
 
 void main() async {
-/*
-  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-      FlutterLocalNotificationsPlugin();
-// initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
-  const AndroidInitializationSettings initializationSettingsAndroid =
-      AndroidInitializationSettings('assets/icon.png');
-//final IOSInitializationSettings initializationSettingsIOS =
-//    IOSInitializationSettings(
-//        onDidReceiveLocalNotification: onDidReceiveLocalNotification);
-  const InitializationSettings initializationSettings = InitializationSettings(
-    android: initializationSettingsAndroid,
-//    iOS: initializationSettingsIOS
-  );
-
-  void selectNotification(String? payload) async {
-    if (payload != null) {
-      debugPrint('notification payload: $payload');
-    }
-  }
-
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await flutterLocalNotificationsPlugin.initialize(initializationSettings,
-      onSelectNotification: selectNotification);
-
-  const AndroidNotificationDetails androidPlatformChannelSpecifics =
-      AndroidNotificationDetails('your channel id', 'your channel name',
-          channelDescription: 'your channel description',
-          importance: Importance.max,
-          priority: Priority.high,
-          ticker: 'ticker');
-  const NotificationDetails platformChannelSpecifics =
-      NotificationDetails(android: androidPlatformChannelSpecifics);
-  await flutterLocalNotificationsPlugin.show(
-      0, 'plain title', 'plain body', platformChannelSpecifics,
-      payload: 'item x');
-*/
   runApp(const MyApp());
 }
 
